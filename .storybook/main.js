cf9529d01cef79f19093ff9ccc2ff3b46e08ac98
Ismael López <Ismael97Lopez@gmail.com>
2023-11-14 21:44:20 -0700

    Componente Simple

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,25 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+module.exports = {
+  stories: ["../src/components/**/*.stories.js"],
   staticDirs: ["../public"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
+
   framework: {
-    name: "@storybook/react-vite",
+    name: "@storybook/react-webpack5",
     options: {},
   },
+
+  features: {
+    interactionsDebugger: true,
+  },
+
   docs: {
-    autodocs: "tag",
+    autodocs: true,
   },
 };
-export default config;
